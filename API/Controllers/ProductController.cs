ff4d983e496d6102102dec6931fda1a0b47e05d3
TruongCongThieu <truongcongthieu134@gmail.com>
2024-06-26 12:27:47 +0700

    init

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using API.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> Products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product 1", Price = 10.0m },
+            new Product { Id = 2, Name = "Product 2", Price = 20.0m }
+        };
+
+        // GET: api/product
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return Products;
+        }
+
+        // GET: api/product/1
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            var product = Products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+        // POST: api/product
+        [HttpPost]
+        public ActionResult<Product> Post([FromBody] Product product)
+        {
+            product.Id = Products.Max(p => p.Id) + 1;
+            Products.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+        }
+
+        // PUT: api/product/1
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Product product)
+        {
+            var existingProduct = Products.FirstOrDefault(p => p.Id == id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+
+            existingProduct.Name = product.Name;
+            existingProduct.Price = product.Price;
+
+            return NoContent();
+        }
+
+        // DELETE: api/product/1
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var product = Products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            Products.Remove(product);
+            return NoContent();
+        }
+    }
+}
